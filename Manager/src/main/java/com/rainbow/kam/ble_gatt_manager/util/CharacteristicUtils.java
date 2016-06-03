6ad7a390872af843d8103b3987114f484b69d9d9
kam6512_rainbow <kam6512@gmail.com>
2016-06-03 11:54:19 +0900

    Characteristic 의 Response 값을 만드는 코드 수정

--- Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/util/CharacteristicUtils.java
+++ Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/util/CharacteristicUtils.java
@@ -7,8 +7,8 @@ import com.google.common.collect.Maps;
 import java.util.HashMap;
 
 import rx.Observable;
-import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by Kang Young Won on 2016-05-20.
@@ -18,31 +18,25 @@ public class CharacteristicUtils {
 
     public static Observable<HashMap<String, String>> getFormattedValues(BluetoothGattCharacteristic characteristic) {
 
-        final String hex = "%02X";
-        final String str = "%c";
-
         final byte[] value = characteristic.getValue();
         final HashMap<String, String> valueMap = Maps.newHashMap();
-        final StringBuilder hexBuilder = new StringBuilder(value.length).append("0x");
+        final StringBuilder hexBuilder = new StringBuilder(value.length);
         final StringBuilder decBuilder = new StringBuilder(value.length);
         final StringBuilder strBuilder = new StringBuilder(value.length);
 
-        return Observable.create(new Observable.OnSubscribe<HashMap<String, String>>() {
-            @Override
-            public void call(Subscriber<? super HashMap<String, String>> subscriber) {
-
-                for (byte byteChar : value) {
-                    hexBuilder.append(String.format(hex, byteChar));
-                    decBuilder.append(byteChar & 0xff);
-                    strBuilder.append(String.format(str, byteChar));
-                }
-                valueMap.put("HEX", hexBuilder.toString());
-                valueMap.put("DEC", decBuilder.toString());
-                valueMap.put("STR", strBuilder.toString());
-
-                subscriber.onNext(valueMap);
-                subscriber.unsubscribe();
+        return Observable.create((Observable.OnSubscribe<HashMap<String, String>>) subscriber -> {
+            for (byte byteChar : value) {
+                hexBuilder.append("0x").append(String.format("%02X", byteChar));
+                decBuilder.append(byteChar & 0xff);
+                strBuilder.append(String.format("%c", byteChar));
             }
-        }).subscribeOn(AndroidSchedulers.mainThread());
+            valueMap.put("HEX", hexBuilder.toString());
+            valueMap.put("DEC", decBuilder.toString());
+            valueMap.put("STR", strBuilder.toString());
+
+            subscriber.onNext(valueMap);
+            subscriber.onCompleted();
+            subscriber.unsubscribe();
+        }).subscribeOn(Schedulers.computation()).observeOn(AndroidSchedulers.mainThread());
     }
 }
