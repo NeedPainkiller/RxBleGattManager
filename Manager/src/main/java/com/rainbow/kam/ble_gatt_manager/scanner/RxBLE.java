a27960b0a018a23f3fd2ab37f79adee7dcd686f5
kam6512_rainbow <kam6512@gmail.com>
2016-06-10 12:16:44 +0900

    스캐닝 클래스 메모리 누수 수정 / 코드 단축

--- Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/scanner/RxBLE.java
+++ Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/scanner/RxBLE.java
@@ -1,6 +1,7 @@
 package com.rainbow.kam.ble_gatt_manager.scanner;
 
 import android.app.Activity;
+import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
@@ -9,117 +10,58 @@ import android.bluetooth.le.ScanResult;
 import android.content.Context;
 
 import com.rainbow.kam.ble_gatt_manager.data.BleDevice;
-import com.rainbow.kam.ble_gatt_manager.helper.BluetoothHelper;
 
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import javax.inject.Inject;
 
 import rx.Observable;
+import rx.subjects.PublishSubject;
 
 /**
- * Created by Kang Young Won on 2016-05-23.
+ * Created by Kang Young Won on 2016-05-24.
  */
 public class RxBle {
 
-    private BluetoothAdapter bluetoothAdapter;
+    private final BluetoothAdapter bluetoothAdapter;
+    private BluetoothLeScanner scanner;
+
+    private PublishSubject<BleDevice> scanSubject = PublishSubject.create();
+
+    private final ScanCallback callback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            scanSubject.onNext(BleDevice.create(result));
+        }
+    };
 
 
-    public RxBle(Activity activity) {
-        String bluetoothService = Context.BLUETOOTH_SERVICE;
-        BluetoothManager manager = (BluetoothManager) activity.getSystemService(bluetoothService);
+    @Inject public RxBle(final Activity activity) {
+        this(activity.getApplication());
+    }
+
+
+    public RxBle(final Application application) {
+        BluetoothManager manager = (BluetoothManager) application.getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = manager.getAdapter();
+        scanner = bluetoothAdapter.getBluetoothLeScanner();
     }
 
 
     public Observable<BleDevice> observe() {
-        if (!BluetoothHelper.IS_BLE_SUPPORTED || !bluetoothAdapter.isEnabled()) {
+        if (!bluetoothAdapter.isEnabled()) {
             return Observable.empty();
         }
-
-        return new Scanner(bluetoothAdapter).observe();
-    }
-
-
-    class Scanner {
-
-        private final BluetoothLeScanner scanner;
-        private final ScanCallbackAdapter callback;
-
-
-        public Scanner(final BluetoothAdapter adapter) {
-            this.scanner = adapter.getBluetoothLeScanner();
-            this.callback = new ScanCallbackAdapter();
-        }
-
-
-        public Observable<BleDevice> observe() {
-//            scanner.startScan(callback);
-//            return callback.toObservable().repeat().distinctUntilChanged().doOnSubscribe(() -> scanner.startScan(callback)).doOnUnsubscribe(() -> scanner.startScan(callback));
-            return callback.toObservable().repeat().distinctUntilChanged().doOnSubscribe(() -> scanner.startScan(callback)).doOnUnsubscribe(() -> scanner.stopScan(callback));
-        }
-
-    }
-
-    class ScanCallbackAdapter extends ScanCallback {
-        private final FutureAdapter futureAdapter = new FutureAdapter();
-
-
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            futureAdapter.setDevice(BleDevice.create(result));
-        }
-
-
-        public Observable<BleDevice> toObservable() {
-            return Observable.from(futureAdapter);
-        }
-    }
-
-    class FutureAdapter implements Future<BleDevice> {
-        private boolean done;
-        private BleDevice bleDevice;
-
-
-        public void setDevice(BleDevice bleDevice) {
-            synchronized (this) {
-                this.bleDevice = bleDevice;
-                this.done = true;
-                this.notify();
+        return Observable.merge(Observable.create((Observable.OnSubscribe<BleDevice>) subscriber -> {
+            if (scanner == null) {
+                scanner = bluetoothAdapter.getBluetoothLeScanner();
             }
-        }
-
-
-        @Override public boolean cancel(boolean mayInterruptIfRunning) {
-            return false;
-        }
-
-
-        @Override public boolean isCancelled() {
-            return false;
-        }
-
-
-        @Override public boolean isDone() {
-            return done;
-        }
-
-
-        @Override public BleDevice get() throws InterruptedException {
-            synchronized (this) {
-                while (bleDevice == null) {
-                    this.wait();
-                }
+            scanner.startScan(callback);
+        }), scanSubject).onBackpressureBuffer().doOnSubscribe(() -> {
+            if (scanner != null && bluetoothAdapter.isEnabled()) {
+                scanner.stopScan(callback);
             }
-            return bleDevice;
-        }
-
-
-        @Override
-        public BleDevice get(long t, TimeUnit u) throws InterruptedException {
-            return get();
-        }
+        });
     }
-
 }
 
 
