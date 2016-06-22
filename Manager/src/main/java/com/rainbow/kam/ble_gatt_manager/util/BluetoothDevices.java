61590b01cff89a9d25e439c420a0cc37a03ae676
kam6512_rainbow <kam6512@gmail.com>
2016-06-22 21:15:27 +0900

    BluetoothDevices.java 코드 수정

--- Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/util/BluetoothDevices.java
+++ Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/util/BluetoothDevices.java
@@ -9,39 +9,38 @@ import com.rainbow.kam.ble_gatt_manager.BuildConfig;
  */
 public class BluetoothDevices {
 
-    private final static String UNKNOWN = BuildConfig.UNKNOWN;
+    public final static String BOND_STATE_NOT_BONDED = "NOT BONDED";
+    public final static String BOND_STATE_BONDING = "BONDING";
+    public final static String BOND_STATE_BONDED = "BONDED";
+    private final static String BOND_STATE_UNKNOWN = BuildConfig.UNKNOWN;
 
-    public final static String NOT_BONDED = "NOT BONDED";
-    public final static String BONDING = "BONDING";
-    public final static String BONDED = "BONDED";
-
-    public final static String CLASSIC = "CLASSIC";
-    public final static String BLE = "BLE";
-    public final static String DUAL = "DUAL";
-
+    public final static String TYPE_CLASSIC = "CLASSIC";
+    public final static String TYPE_BLE = "BLE";
+    public final static String TYPE_DUAL = "DUAL";
+    private final static String TYPE_UNKNOWN = BuildConfig.UNKNOWN;
 
     private final static SparseArray<String> BOND_LIST = new SparseArray<>();
     private final static SparseArray<String> TYPE_LIST = new SparseArray<>();
 
 
     public static String getBond(int bond) {
-        return BOND_LIST.get(bond, UNKNOWN);
+        return BOND_LIST.get(bond, BOND_STATE_UNKNOWN);
     }
 
 
     public static String getType(int type) {
-        return TYPE_LIST.get(type, UNKNOWN);
+        return TYPE_LIST.get(type, TYPE_UNKNOWN);
     }
 
 
     static {
-        BOND_LIST.put(10, NOT_BONDED);
-        BOND_LIST.put(11, BONDING);
-        BOND_LIST.put(12, BONDED);
+        BOND_LIST.put(10, BOND_STATE_NOT_BONDED);
+        BOND_LIST.put(11, BOND_STATE_BONDING);
+        BOND_LIST.put(12, BOND_STATE_BONDED);
 
-        TYPE_LIST.put(0, UNKNOWN);
-        TYPE_LIST.put(1, CLASSIC);
-        TYPE_LIST.put(2, BLE);
-        TYPE_LIST.put(3, DUAL);
+        TYPE_LIST.put(0, TYPE_UNKNOWN);
+        TYPE_LIST.put(1, TYPE_CLASSIC);
+        TYPE_LIST.put(2, TYPE_BLE);
+        TYPE_LIST.put(3, TYPE_DUAL);
     }
 }
