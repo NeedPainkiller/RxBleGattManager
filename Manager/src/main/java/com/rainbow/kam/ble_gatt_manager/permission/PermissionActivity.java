0399dfcba040531d173e76dadfa81b12078c3eba
kam6512_rainbow <kam6512@gmail.com>
2016-08-30 12:18:51 +0900

    PermissionActivity 수정
    PermissionActivity.java :
        - setActivityWindow / setPermissionBundle / setPermissionResources 생성
        - 일부 코드 수정

--- Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/permission/PermissionActivity.java
+++ Manager/src/main/java/com/rainbow/kam/ble_gatt_manager/permission/PermissionActivity.java
@@ -2,7 +2,6 @@ package com.rainbow.kam.ble_gatt_manager.permission;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -17,19 +16,60 @@ import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
 
-/**
- * Created by TedPark on 16. 2. 17..
- */
+import static android.content.pm.PackageManager.PERMISSION_DENIED;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import static com.rainbow.kam.ble_gatt_manager.permission.AndroidPermission.KEY_DENIED_CLOSE;
+import static com.rainbow.kam.ble_gatt_manager.permission.AndroidPermission.KEY_DENIED_MSG;
+import static com.rainbow.kam.ble_gatt_manager.permission.AndroidPermission.KEY_EXPLANATION_CONFIRM;
+import static com.rainbow.kam.ble_gatt_manager.permission.AndroidPermission.KEY_EXPLANATION_MSG;
+import static com.rainbow.kam.ble_gatt_manager.permission.AndroidPermission.KEY_PACKAGE_NAME;
+import static com.rainbow.kam.ble_gatt_manager.permission.AndroidPermission.KEY_PERMISSIONS;
+
 public class PermissionActivity extends AppCompatActivity {
 
     private static final int REQ_CODE_PERMISSION_REQUEST = 10;
     private static final int REQ_CODE_REQUEST_SETTING = 20;
 
+    private Bundle permissionBundle;
+
+    private String packageName;
+    private String[] permissions;
+
+    private String explanationMessage;
+    private String explanationConfirmText;
+
+    private String deniedMessage;
+    private String deniedCloseButtonText;
+    private String showSettingButtonText;
+
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        setActivityWindow();
+        setPermissionBundle();
+        setPermissionResources();
         checkPermissions(false);
+    }
+
+
+    private void setActivityWindow() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+    }
+
+
+    private void setPermissionBundle() {
+        permissionBundle = getIntent().getExtras();
+    }
+
+
+    private void setPermissionResources() {
+        packageName = permissionBundle.getString(KEY_PACKAGE_NAME);
+        permissions = permissionBundle.getStringArray(KEY_PERMISSIONS);
+        explanationMessage = permissionBundle.getString(KEY_EXPLANATION_MSG);
+        explanationConfirmText = permissionBundle.getString(KEY_EXPLANATION_CONFIRM);
+        deniedMessage = permissionBundle.getString(KEY_DENIED_MSG);
+        deniedCloseButtonText = permissionBundle.getString(KEY_DENIED_CLOSE);
+        showSettingButtonText = permissionBundle.getString(KEY_DENIED_MSG);
     }
 
 
@@ -47,8 +87,8 @@ public class PermissionActivity extends AppCompatActivity {
         ArrayList<String> needPermissions = new ArrayList<>();
         boolean showRationale = false;
 
-        for (String permission : AndroidPermission.permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PERMISSION_GRANTED) {
                 needPermissions.add(permission);
                 showRationale = true;
             }
@@ -58,7 +98,7 @@ public class PermissionActivity extends AppCompatActivity {
             permissionGranted();
         } else if (fromOnActivityResult) {
             permissionDenied(needPermissions);
-        } else if (showRationale && !TextUtils.isEmpty(AndroidPermission.explanationMessage)) {
+        } else if (showRationale && !TextUtils.isEmpty(explanationMessage)) {
             showRationaleDialog(needPermissions);
         } else {
             requestPermissions(needPermissions);
@@ -71,9 +111,8 @@ public class PermissionActivity extends AppCompatActivity {
         ArrayList<String> deniedPermissions = new ArrayList<>();
 
         for (int i = 0; i < permissions.length; i++) {
-            String permission = permissions[i];
-            if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
-                deniedPermissions.add(permission);
+            if (grantResults[i] == PERMISSION_DENIED) {
+                deniedPermissions.add(permissions[i]);
             }
         }
 
@@ -86,14 +125,14 @@ public class PermissionActivity extends AppCompatActivity {
 
 
     private void permissionGranted() {
-        AndroidPermission.permissionSubject.onCompleted();
+        AndroidPermission.listener.permissionGranted();
         finish();
         overridePendingTransition(0, 0);
     }
 
 
     private void permissionDenied(ArrayList<String> deniedPermissions) {
-        AndroidPermission.permissionSubject.onNext(deniedPermissions);
+        AndroidPermission.listener.permissionDenied(deniedPermissions);
         finish();
         overridePendingTransition(0, 0);
     }
@@ -106,9 +145,9 @@ public class PermissionActivity extends AppCompatActivity {
 
     private void showRationaleDialog(final ArrayList<String> needPermissions) {
         new MaterialDialog.Builder(this)
-                .content(AndroidPermission.explanationMessage)
+                .content(explanationMessage)
                 .cancelable(false)
-                .negativeText(AndroidPermission.explanationConfirmText)
+                .negativeText(explanationConfirmText)
                 .onNegative((dialog, which) -> requestPermissions(needPermissions))
                 .show();
     }
@@ -116,19 +155,19 @@ public class PermissionActivity extends AppCompatActivity {
 
     private void showPermissionDenyDialog(final ArrayList<String> deniedPermissions) {
         new MaterialDialog.Builder(this)
-                .content(AndroidPermission.denyMessage)
+                .content(deniedMessage)
                 .cancelable(false)
-                .positiveText(AndroidPermission.settingButtonText)
+                .positiveText(showSettingButtonText)
                 .onPositive((dialog, which) -> {
                     Intent intent;
                     try {
-                        intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + AndroidPermission.packageName));
+                        intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.parse("package:" + packageName));
                     } catch (ActivityNotFoundException e) {
                         intent = new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
                     }
                     startActivityForResult(intent, REQ_CODE_REQUEST_SETTING);
                 })
-                .negativeText(AndroidPermission.deniedCloseButtonText)
+                .negativeText(deniedCloseButtonText)
                 .onNegative((dialog, which) -> permissionDenied(deniedPermissions)).show();
     }
 }
